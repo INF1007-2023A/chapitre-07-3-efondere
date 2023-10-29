faccf4dbe4da9a99f4a11c460e2ec31c03f0868c
efondere <97473864+efondere@users.noreply.github.com>
2023-10-29 19:01:49 -0400

    terminer les exercices du chapitre 7.3

--- README.md
+++ README.md
@@ -8,12 +8,20 @@ Avant de commencer. Consulter les instructions à suivre dans [instructions.md](
 
 ## Standard MIDI
 
-Dans cette série d'exercices, nous utiliserons un clavier MIDI virtuel nous permettant de produire des notes musicales et la [librairie Mido](https://mido.readthedocs.io/en/latest/) en Python. Elle ne fait pas partie de la libraire standard de Python, il faut donc l'installer (soit avec `pip` ou à travers votre IDE). Pour nos besoin, disons seulement que le standard MIDI est un système de messages permettant de savoir quelles notes du clavier sont appuyées et relâchées. La librairie Mido nous permet d'obtenir ces messages dans un code Python et d'effectuer des actions sur ceux-ci.
+Dans cette série d'exercices, nous utiliserons un clavier MIDI virtuel nous permettant de produire 
+des notes musicales et la [librairie Mido](https://mido.readthedocs.io/en/latest/) en Python. Elle
+ne fait pas partie de la libraire standard de Python, il faut donc l'installer (soit avec `pip` ou
+à travers votre IDE). Pour nos besoin, disons seulement que le standard MIDI est un système de
+messages permettant de savoir quelles notes du clavier sont appuyées et relâchées. La librairie Mido
+nous permet d'obtenir ces messages dans un code Python et d'effectuer des actions sur ceux-ci.
 
 ## 1. Associations entre numéro MIDI et nom de notes
 ### `build_note_dictionaries`
 
-Écrivez une fonction qui génère deux dictionnaires : un qui associe chaque numéro MIDI pertinent à un nom de note ("Do", "Ré", "C", "D", etc.) et un qui fait l'inverse. la fonction prend en paramètre le nom de chaque 12 notes de l'octave partant sur do. Le deuxième paramètre indique si les noms de notes doivent être suivis du numéro de l'octave (par exemple "C4" ou "C" pour le *middle C*).
+Écrivez une fonction qui génère deux dictionnaires : un qui associe chaque numéro MIDI pertinent à
+un nom de note ("Do", "Ré", "C", "D", etc.) et un qui fait l'inverse. la fonction prend en paramètre
+le nom de chaque 12 notes de l'octave partant sur do. Le deuxième paramètre indique si les noms de
+notes doivent être suivis du numéro de l'octave (par exemple "C4" ou "C" pour le *middle C*).
 
 Exemple :
 ```python
@@ -39,9 +47,19 @@ Réb
 ## 2. Afficher le nom des notes jouées
 ### `build_print_note_name_callback`
 
-Écrivez une fonction qui retourne une fonction qui sert rappel (*callback*) et qui affiche le nom des notes qui sont jouées. On affiche seulement quand la note est appuyée, pas relâchée. On passe en paramètre à `build_print_note_name_callback` le dictionnaire d'associations de numéros MIDI à noms (le premier dictionnaire retourné par `build_note_dictionaries`).
+Écrivez une fonction qui retourne une fonction qui sert rappel (*callback*) et qui affiche le nom
+des notes qui sont jouées. On affiche seulement quand la note est appuyée, pas relâchée. On passe
+en paramètre à `build_print_note_name_callback` le dictionnaire d'associations de numéros MIDI à
+noms (le premier dictionnaire retourné par `build_note_dictionaries`).
 
-Avec la librairie Mido, on peut assigner une fonction de rappel pour traiter les messages quand ils arrivent. Les callback de messages doivent être des fonctions (ou des objets fonctionnels) qui prennent en paramètre un message MIDI. On peut obtenir le type d'un message ([documentation](https://mido.readthedocs.io/en/latest/message_types.html)) via son `.type`. Si le type est `note_on`, on peut obtenir sa vitesse (le volume de la note) via son `.velocity`. On considère qu'une note est appuyées quand on a un message de type `note_on` avec une vitesse supérieure à 0. Une note est relâchée quand on a un `note_off` ou un `note_on` avec une vitesse de 0 (certains claviers envoient un `note_on` de volume 0 au lieu d'un `note_off`).
+Avec la librairie Mido, on peut assigner une fonction de rappel pour traiter les messages quand ils
+arrivent. Les callback de messages doivent être des fonctions (ou des objets fonctionnels) qui
+prennent en paramètre un message MIDI. On peut obtenir le type d'un message
+([documentation](https://mido.readthedocs.io/en/latest/message_types.html)) via son `.type`. Si le
+type est `note_on`, on peut obtenir sa vitesse (le volume de la note) via son `.velocity`. On
+considère qu'une note est appuyées quand on a un message de type `note_on` avec une vitesse
+supérieure à 0. Une note est relâchée quand on a un `note_off` ou un `note_on` avec une vitesse de
+0 (certains claviers envoient un `note_on` de volume 0 au lieu d'un `note_off`).
 
 Voir exemple ci-dessous pour ouverture d'un port MIDI et enregistrement d'un callback.
 
@@ -70,7 +88,12 @@ Do5
 ## 3. Afficher les noms d'accords
 ### `build_print_chord_name_callback`
 
-Écrivez une fonction qui retourne une fonction qui sert callback et qui affiche le nom des accords qui sont joués. On affiche seulement quand seulement les notes de l'accord sont appuyées, pas relâchées, et on ne s'occupe pas des arpèges. On passe en paramètre à `build_print_chord_name_callback` un dictionnaire d'associations notes et de nom d'accords suivi d'un dictionnaire de noms à numéro MIDI (le deuxième dictionnaire retourné par `build_note_dictionaries`).
+Écrivez une fonction qui retourne une fonction qui sert callback et qui affiche le nom des accords
+qui sont joués. On affiche seulement quand seulement les notes de l'accord sont appuyées, pas
+relâchées, et on ne s'occupe pas des arpèges. On passe en paramètre à
+`build_print_chord_name_callback` un dictionnaire d'associations notes et de nom d'accords suivi
+d'un dictionnaire de noms à numéro MIDI (le deuxième dictionnaire retourné par
+`build_note_dictionaries`).
 
 Exemple :
 ```python
--- exercice.py
+++ exercice.py
@@ -8,65 +8,121 @@ NOTES_PER_OCTAVE = 12
 
 
 def build_note_dictionaries(note_names, add_octave_no=True):
-	C0_MIDI_NO = 12 # Plus basse note sur les pianos est La 0, mais on va commencer à générer les noms sur Do 0
+    C0_MIDI_NO = 12  # Plus basse note sur les pianos est La 0, mais on va commencer à générer les noms sur Do 0
 
-	midi_to_name = {}
-	name_to_midi = {}
-	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
-		# Pour chaque note de l'octave
-			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
-			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
-			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
-	return midi_to_name, name_to_midi
+    midi_to_name = {}
+    name_to_midi = {}
+    # Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
+    for octave in range(9):
+        # Pour chaque note de l'octave
+        for note in range(NOTES_PER_OCTAVE):
+            # Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
+            midi_number = C0_MIDI_NO + octave * NOTES_PER_OCTAVE + note
+            # Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
+            note_name = note_names[note] + (str(octave) if add_octave_no else "")
+            midi_to_name[midi_number] = note_name
+            # Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
+            name_to_midi[note_name] = (
+                midi_number if add_octave_no else midi_number % NOTES_PER_OCTAVE
+            )
+    return midi_to_name, name_to_midi
+
 
 def build_print_note_name_callback(midi_to_name):
-	pass
+    def note_callback(midi_message):
+        if midi_message.type == "note_on" and midi_message.velocity > 0:
+            print(midi_to_name[midi_message.note])
+
+    return note_callback
+
 
 def build_print_chord_name_callback(chord_names_and_notes, name_to_midi):
-	# Construire le dictionnaire d'assocations entre état des notes et accord joué.
-	
-	# Créez et retourner le callback
-	pass
+    chords_status = {}
+    for name in chord_names_and_notes:
+        chords_status[name] = {}
+        for note in chord_names_and_notes[name]:
+            chords_status[name][name_to_midi[note]] = False
+
+    def chord_callback(message):
+        if message.type != "note_off" and message.type != "note_on":
+            return
+
+        is_on = message.type == "note_on" and message.velocity > 0
+
+        for chord, notes in chords_status.items():
+            for note in notes:
+                if note == message.note:
+                    if not is_on and False not in notes.values():
+                        print("Stopping chord", chord)
+
+                    chords_status[chord][note] = is_on
+
+                    if is_on and False not in notes.values():
+                        print("Starting chord", chord)
+
+    return chord_callback
 
 
 def main():
-	PORT_MIDI = "UnPortMIDI 0"
+    PORT_MIDI = "Midi Through:Midi Through Port-0 14:0"
 
-	english_names = ["C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
-	solfeggio_names = ["Do", "Réb", "Ré", "Mib", "Mi", "Fa", "Fa#", "Sol", "Lab", "La", "Sib", "Si"]
+    english_names = ["C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
+    solfeggio_names = [
+        "Do",
+        "Réb",
+        "Ré",
+        "Mib",
+        "Mi",
+        "Fa",
+        "Fa#",
+        "Sol",
+        "Lab",
+        "La",
+        "Sib",
+        "Si",
+    ]
 
-	midi_to_name_eng_8va, name_to_midi_eng_8va = build_note_dictionaries(english_names, True)
-	midi_to_name_fr, name_to_midi_fr = build_note_dictionaries(solfeggio_names, False)
-	print(midi_to_name_eng_8va[64])
-	print(name_to_midi_eng_8va["C0"])
-	print(midi_to_name_fr[61])
-	print(midi_to_name_fr[73])
-	print(name_to_midi_fr["Fa#"])
+    midi_to_name_eng_8va, name_to_midi_eng_8va = build_note_dictionaries(
+        english_names, True
+    )
+    midi_to_name_fr, name_to_midi_fr = build_note_dictionaries(solfeggio_names, False)
+    print(midi_to_name_eng_8va[64])
+    print(name_to_midi_eng_8va["C0"])
+    print(midi_to_name_fr[61])
+    print(midi_to_name_fr[73])
+    print(name_to_midi_fr["Fa#"])
 
-	input("Appuyez sur ENTER pour passer à l'étape suivante...")
-	print("- - " * 30)
-	
-	midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, True)
-	print_note_name = build_print_note_name_callback(midi_to_name)
-	keyboard = mido.open_input(PORT_MIDI, callback=print_note_name)
+    input("Appuyez sur ENTER pour passer à l'étape suivante...")
+    print("- - " * 30)
 
-	input("Affichage des noms de notes (Appuyez sur ENTER pour passer à l'étape suivante)..." "\n")
-	keyboard.close()
+    midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, True)
+    print_note_name = build_print_note_name_callback(midi_to_name)
+    keyboard = mido.open_input(PORT_MIDI, callback=print_note_name)
 
-	print("- - " * 30)
+    input(
+        "Affichage des noms de notes (Appuyez sur ENTER pour passer à l'étape suivante)..."
+        "\n"
+    )
+    keyboard.close()
 
-	chord_names = {
-		"Do majeur" : ("Do", "Mi", "Sol"),
-		"Fa majeur" : ("Fa", "La", "Do"),
-		"Sol majeur" : ("Sol", "Si", "Ré"),
-	}
-	
-	midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, False)
-	print_chord_name = build_print_chord_name_callback(chord_names, name_to_midi)
-	keyboard = mido.open_input(PORT_MIDI, callback=print_chord_name)
-	
-	input("Affichage des noms d'accords (Appuyez sur ENTER pour passer à l'étape suivante)..." "\n")
-	keyboard.close()
+    print("- - " * 30)
+
+    chord_names = {
+        "Do majeur": ("Do3", "Mi3", "Sol3"),
+        "Fa majeur": ("Fa3", "La3", "Do3"),
+        "Sol majeur": ("Sol3", "Si2", "Ré3"),
+    }
+
+    # midi_to_name, name_to_midi = build_note_dictionaries(solfeggio_names, False)
+    print_chord_name = build_print_chord_name_callback(chord_names, name_to_midi)
+    keyboard = mido.open_input(PORT_MIDI, callback=print_chord_name)
+
+    input(
+        "Affichage des noms d'accords (Appuyez sur ENTER pour passer à l'étape suivante)..."
+        "\n"
+    )
+    keyboard.close()
+
 
 if __name__ == "__main__":
-	main()
+    main()
